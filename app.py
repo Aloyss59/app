58989d35c4aff5b99d2edbf883741b78de5486d2
Charlxy <charly59000@orange.fr>
2024-04-20 15:05:56 +0200

    ajout du css

--- app.py
+++ app.py
@@ -7,9 +7,14 @@ app = Flask(__name__, template_folder='', static_folder='')
 def home():
     return render_template("home.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 @app.errorhandler(404)
 def page_not_found(e):
     @app.route("/404")
     def page_404():
         return render_template('404.html')
     
+app.run(debug=True, host='0.0.0.0', port=5000)
