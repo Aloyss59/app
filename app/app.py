40727640a089c4c068e59ecbb221cea2dedef7cb
Charlxy <charly59000@orange.fr>
2024-05-20 20:37:30 +0200

    mise a jour app.py et ajout des pages de connexion et d'enregistrement

--- app/app.py
+++ app/app.py
@@ -5,10 +5,42 @@ app = Flask(__name__, template_folder='./flaskr/templates', static_folder='./fla
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("1erpage.html")
+    return render_template("chat.html")
+
+@app.route('/appareil-photo')
+def appareil_photo():
+    return render_template("photo.html")
+
+@app.route('/amis')
+def amis():
+    return render_template("amis.html")
+
+@app.route('/album')
+def album():
+    return render_template("album.html")
+
+@app.route('/discussion')
+def discussion():
+    return render_template("discussion.html")
+
+@app.route('/discussion/parametre')
+def parametre():
+    return render_template("parametres.html")
+
+@app.route('/parametre')
+def reglage():
+    return render_template("reglage.html")
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
     
 app.run(debug=True, host='0.0.0.0', port=5000)
