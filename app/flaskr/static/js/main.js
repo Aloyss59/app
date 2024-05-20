80e3f632940429244d432d4fec911e493b12eeb4
Alo_59 <sourdin.aloys@gmail.com>
2024-05-20 20:32:38 +0200

    lien faire main.js

--- app/flaskr/static/js/main.js
+++ app/flaskr/static/js/main.js
@@ -1,8 +1,21 @@
 function handleReglageClick() {
-    if (confirm('Voulez-vous aller à la page Google ?')) {
-        window.location.href = 'https://www.google.com';
-    }
-}
+    window.location.href = '../templates/reglage.html';
+}  
 function redirectToChatPage() {
-    window.location.href = "url_de_votre_page_html";
+    window.location.href = "../templates/discussion.html";
+}
+function pagealbum() {
+    window.location.href = "../templates/album.html";
+}
+function pageamis() {
+    window.location.href = "../templates/amis.html";
+}
+function pagecam() {
+    window.location.href = "../templates/photo.html";
+}
+function pagechat() {
+    window.location.href = "../templates/chat.html";
+}
+function pageparametre() {
+    window.location.href = "../templates/parametres.html";
 }
