4936bb31110fe2f6befa0a9b472b737402485d65
Alo_59 <sourdin.aloys@gmail.com>
2024-05-20 17:48:52 +0200

    amélioration des dossier

---
+++ app/flaskr/static/js/main.js
@@ -0,0 +1,8 @@
+function handleReglageClick() {
+    if (confirm('Voulez-vous aller à la page Google ?')) {
+        window.location.href = 'https://www.google.com';
+    }
+}
+function redirectToChatPage() {
+    window.location.href = "url_de_votre_page_html";
+}
