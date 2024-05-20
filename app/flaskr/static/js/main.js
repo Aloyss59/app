8a65cc7ddd36e57d5ff5234d6175078518d1d0e6
Alo_59 <sourdin.aloys@gmail.com>
2024-05-20 21:45:15 +0200

    ok

--- app/flaskr/static/js/main.js
+++ app/flaskr/static/js/main.js
@@ -1,27 +1,21 @@
 function handleReglageClick() {
-    window.location.href = '../templates/reglage.html';
+    window.location.href = '/parametre';
 }  
 function redirectToChatPage() {
-    window.location.href = "../templates/discussion.html";
+    window.location.href = "/";
 }
 function pagealbum() {
-    window.location.href = "../templates/album.html";
+    window.location.href = "/album";
 }
 function pageamis() {
-    window.location.href = "../templates/amis.html";
+    window.location.href = "/amis";
 }
 function pagecam() {
-    window.location.href = "../templates/photo.html";
+    window.location.href = "/appareil-photo";
 }
 function pagechat() {
-    window.location.href = "../templates/chat.html";
+    window.location.href = "/discussion";
 }
 function pageparametre() {
-    window.location.href = "../templates/parametres.html";
-}
-function redirectToChatPage() {
-    document.body.classList.add('slide-out');
-    setTimeout(function() {
-        window.location.href = "../templates/pagediscussion.html";
-    }, 500); // Attendre la fin de l'animation (0.5s)
+    window.location.href = "/discussion/parametre";
 }
