38462cb2656bd7612970aecb44b084b0f5ce664c
Alo_59 <sourdin.aloys@gmail.com>
2024-05-21 21:17:48 +0200

    reparage du js

--- app/flaskr/static/js/navigation.js
+++ app/flaskr/static/js/navigation.js
@@ -1,24 +1,3 @@
-function handleReglageClick() {
-    window.location.href = '/parametre';
-}  
-function redirectToChatPage() {
-    window.location.href = "/";
-}
-function pagealbum() {
-    window.location.href = "/album";
-}
-function pageamis() {
-    window.location.href = "/amis";
-}
-function pagecam() {
-    window.location.href = "/appareil-photo";
-}
-function pagechat() {
-    window.location.href = "/";
-}
-function pageparametre() {
-    window.location.href = "/discussion/parametre";
-}
 document.addEventListener('keydown', function(event) {
     const page = window.location.pathname.split('/').pop();
 
