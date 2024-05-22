e33f8021f458da6c72849a4784b73610fe11829e
Charlxy <charly59000@orange.fr>
2024-05-22 22:53:18 +0200

    ajout des parametre temporaire

--- app/flaskr/static/js/main.js
+++ app/flaskr/static/js/main.js
@@ -19,3 +19,13 @@ function pagechat() {
 function pageparametre() {
     window.location.href = "/discussion/parametre";
 }
+
+document.getElementById('toggle-switch').addEventListener('change', function() {
+    if (this.checked) {
+        console.log('Le switch est activé');
+        // Ajouter ici ce qui doit se passer quand le switch est activé
+    } else {
+        console.log('Le switch est désactivé');
+        // Ajouter ici ce qui doit se passer quand le switch est désactivé
+    }
+});
