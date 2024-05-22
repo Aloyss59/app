394fa67a1931a00b0ce0d0b0b2218e35272229f3
Alo_59 <sourdin.aloys@gmail.com>
2024-05-22 22:49:44 +0200

    amiss

--- app/flaskr/static/js/main.js
+++ app/flaskr/static/js/main.js
@@ -19,3 +19,24 @@ function pagechat() {
 function pageparametre() {
     window.location.href = "/discussion/parametre";
 }
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+window.onload = function() {
+    var buttons = document.querySelectorAll('.bonhomme-button');
+    buttons.forEach(function(button) {
+        var color = getRandomColor();
+        button.style.backgroundColor = color;
+        button.style.position = 'relative'; // Assure que le pseudo-élément est positionné par rapport à ce conteneur
+        var beforeElement = document.createElement('div'); // Crée un nouvel élément pour le pseudo-élément
+        beforeElement.classList.add('bonhomme-button-before'); // Ajoute la classe au nouvel élément
+        beforeElement.style.backgroundColor = color; // Applique la couleur aléatoire au pseudo-élément
+        button.appendChild(beforeElement); // Ajoute le pseudo-élément en tant qu'enfant du bouton
+    });
+};
