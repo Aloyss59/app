e99e2c4529f8b529ded686ee69f819db646cf580
Charlxyz <deleted.user.0065@gmail.com>
2024-08-20 18:55:13 +0200

    prise en charge de l'erreur serveur 500 et ajout de l'image avec

--- alpha/app.py
+++ alpha/app.py
@@ -541,6 +541,10 @@ def get_usernames():
 def page_not_found(e):
     return render_template('404.html')
 
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html')
+
 @app.route('/upload', methods=['POST'])
 @login_required
 def upload():
